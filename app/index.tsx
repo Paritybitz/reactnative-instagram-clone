9c7866a607de3d99cdc5043d3864fe2ad861830c
Ali Mohsin <ali.mohsin05@gmail.com>
2025-06-09 20:47:59 -0400

    images

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,16 @@
-import { View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { styles } from "../styles/auth.styles";
 
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.title}>Title</Text>
+      <Text style={styles.title}>Title Style</Text>
       
       <Image source={{
         uri: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1094874726.png?crop=1.00xw:0.753xh;0,0.161xh&resize=1200:*"}} 
         style={styles.image} />
-       */}
+      
       {/* <TouchableOpacity onPress={() => alert("You touched")}>
         <Text>Press mee!</Text>
       </TouchableOpacity> */}
