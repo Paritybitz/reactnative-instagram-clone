456eaa6444ff3f7ce39f85e68de8115846964c16
Ali Mohsin <ali.mohsin05@gmail.com>
2025-06-12 19:13:23 -0400

    auth

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../../styles/auth.styles";
+
+export default function index() {
+  return (
+    <View style={styles.container}>
+      
+      <Text style={styles.title}>Title</Text>
+
+      <Image
+        source={{
+          uri: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1094874726.png?crop=1.00xw:0.753xh;0,0.161xh&resize=1200:*",
+        }}
+        style={styles.image}
+      />
+
+      <TouchableOpacity onPress={() => alert("You touched")}>
+        <Text>Press mee!</Text>
+      </TouchableOpacity>
+
+      <Link href={"/notifications"}>Feed screen in tabs</Link>
+
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,5 @@
-import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity, View } from "react-native";
-import { styles } from "../styles/auth.styles";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      
-      <Text style={styles.title}>Title</Text>
-
-      <Image
-        source={{
-          uri: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1094874726.png?crop=1.00xw:0.753xh;0,0.161xh&resize=1200:*",
-        }}
-        style={styles.image}
-      />
-
-      <TouchableOpacity onPress={() => alert("You touched")}>
-        <Text>Press mee!</Text>
-      </TouchableOpacity>
-
-      <Link href={"/notifications"}>Visit Notifications Screen</Link>
-
-    </View>
-  );
+  return <Redirect href="/(auth)/login" />;
 }
